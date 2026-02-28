2c53e7b3c10e7f61c5d272074e1f169a01e1cd6d
Neha Avarachan <avrneha99@gmail.com>
2026-02-28 07:42:02 +0530

    Prepare for deployment

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from database import db, User, BloodRequest, DonorResponse
 from datetime import datetime, timedelta
 from app import app
 
-BOT_TOKEN = "8440309378:AAFMT7nVeGbykvR2NE6clS8lNPgNhviGy4A"
+BOT_TOKEN = "YOUR_TOKEN_HERE"
 
 # --- /start command ---
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
